649dcb79d4abae9f50ba09fd731a5f006b5fdac8
Nirav Sanghadiya <nirav.sanghadiya8@gmail.com>
2025-01-25 22:37:27 +0530

    Added Singleton Pattern

---
+++ Singleton Pattern/DbConnection.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton_Pattern
+{
+    public sealed class DbConnection
+    {
+        // Static instance
+        private static DbConnection connectionInstance;
+
+        private static readonly object lockObject = new object();
+
+        // Private Consctructor
+        private DbConnection() { }
+
+        // Static method to return instance
+        public static DbConnection GetInstance()
+        {
+            if(connectionInstance == null)
+            {
+                lock (lockObject)
+                {
+                    if (connectionInstance == null)
+                    {
+                        connectionInstance = new DbConnection();
+                    }
+                }              
+            }
+           
+            return connectionInstance;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"This is a Singleton class");
+        }
+    }
+}
---
+++ Singleton Pattern/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Singleton_Pattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            DbConnection dbConnection1 = DbConnection.GetInstance();
+            DbConnection dbConnection2 = DbConnection.GetInstance();
+            if(dbConnection1 == dbConnection2)
+            {
+                Console.WriteLine($"Instances are same");
+            }
+            //dbConnection1.Display();
+        }
+    }
+}
